8c767e0abc94dc0a41f4d05e148507d32b39cbc3
arunpiku <giri.arun592@gmail.com>
2018-09-15 00:47:26 +0530

    regular expression p2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # regularExpression
 A regular expression is a special text string for describing a search pattern. You can think of regular expressions as wildcards on steroids. You are probably familiar with wildcard notations such as *.txt to find all text files in a file manager. The regex equivalent is .*\.txt.
+
+Build your regular expression online:-
+	https://www.regextester.com/
+	https://regex101.com/
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+//Example: (^[a-z]+$)
+
+echo "preg_match<br><br>";
+
 $name = "Arun Giri";
 print_r("Name: ".preg_match("(^[a-z A-Z]{4,15}+$)", $name));
 echo "<br><br>";
@@ -13,5 +17,25 @@ echo "<br><br>";
 
 $email = "technicaldebug@gmail.com";
 print_r("Email: ".preg_match("(^[a-zA-Z0-9.]+@[a-z]+.[a-z]+$)", $email));
+echo "<br><br>";
 
+$address = "Contai, West Bengal.";
+print_r("Address: ".preg_match("(^[a-zA-Z 0-9,.]{1,50}+$)", $address));
+echo "<br><br>";
+
+$url = "a";
+print_r("Url: ".preg_match("(^[a-zA-Z 0-9:\/.?=+&_%#]+$)", $url));
+echo "<br><br>";
+
+echo "preg_split<br><br>";
+
+$split = "arun giri,1234567895,arungiri@12345,technicaldebug@gmail.com,Contai, West Bengal";
+print_r(preg_split("/,/", $split));
+echo "<br><br>";
+
+echo "Preg_replace<br><br>";
+
+$replace = "Hello world, welcome to Technical Debug.";
+print_r(Preg_replace("/l/","x", $replace));
+echo "<br><br>";
 ?>
